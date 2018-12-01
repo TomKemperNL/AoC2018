08023a30c15b2490de0e1b53f92c6cc01582f95f
Tom Kemper <tomkempernl@gmail.com>
2018-12-01 20:54:41 +0100

    AoC Day 1, part B is not quite FSharpy

---
+++ AoC2018.Tests/Day1Tests.fs
@@ -0,0 +1,24 @@
+﻿module AoC2018.Tests.Day1
+
+open NUnit.Framework.Internal
+open NUnit.Framework
+open Day1
+
+[<TestFixture>]
+type Class1() =     
+    
+    [<TestCase(3, "+1, -2, +3, +1")>]
+    [<TestCase(3, "+1, +1, +1")>]
+    [<TestCase(0, "+1, +1, -2")>]
+    [<TestCase(-6, "-1, -2, -3")>]
+    member this.A(exp, input) =
+       Assert.AreEqual(exp, (day1AExamples input))
+
+    
+    [<TestCase(2, "+1, -2, +3, +1")>]
+    [<TestCase(0, "+1, -1")>]
+    [<TestCase(10, "+3, +3, +4, -2, -4")>]
+    [<TestCase(5, "-6, +3, +8, +5, -6")>]
+    [<TestCase(14, "+7, +7, -2, -7, -4")>]
+    member this.B(exp, input) =
+       Assert.AreEqual(exp, (day1BExamples input))    
---
+++ AoC2018/Day1.fs
@@ -0,0 +1,64 @@
+﻿module Day1
+
+open System
+open System.Collections.Generic
+
+let parseToken (s:String) =    
+    match s with
+    | null -> id
+    | "0" -> id
+    | s when s.Length < 2 -> id
+    | s ->
+        let token = s.Chars 0
+        let success, value = Int32.TryParse (s.Substring 1)
+        match success, token, value with
+        | false, _, _ -> id
+        | true, '+', x -> 
+            fun(a) -> a + x
+        | true, '-', x -> 
+            fun(a) -> a - x
+        | true, _, _ -> id
+
+let day1A (inputs: String[]) = 
+        inputs
+        |> Array.map (fun s -> s.Trim())
+        |> Array.map parseToken 
+        |> Array.fold (|>) 0 
+
+let processInput (collector: List<int>) (input, alreadyFoundIt) (operator: int->int) =
+    let result = operator input
+    match alreadyFoundIt with
+    | Some x -> 
+        (result, Some x)
+    | None ->
+        let foundIt = 
+            if collector.Contains(result) 
+            then Some(result) 
+            else
+                collector.Add(result)
+                None
+        (result, foundIt)
+
+let day1B (inputs: String[]) =    
+    let seenValues = new List<int>()
+    seenValues.Add(0)
+
+    let mutable current: int = 0
+    let mutable result: int option = None
+
+    while result = None do
+        let (newint, foundIt) = 
+            inputs
+                |> Array.map (fun s -> s.Trim())
+                |> Array.map parseToken 
+                |> Array.fold (processInput seenValues) (current, result) 
+        current <- newint
+        result <- foundIt
+    result.Value
+
+let day1AExamples (s: string) =
+    s.Split(',') |> day1A
+
+let day1BExamples (s: string) =
+    s.Split(',') |> day1B
+
