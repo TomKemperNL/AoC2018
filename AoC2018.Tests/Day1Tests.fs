9b4aac1e1fa5e7807ee909c14551d2148dbdeeac
Tom Kemper <tomkempernl@gmail.com>
2018-12-01 22:33:13 +0100

    Finally

--- AoC2018.Tests/Day1Tests.fs
+++ AoC2018.Tests/Day1Tests.fs
@@ -1,24 +1,36 @@
 ﻿module AoC2018.Tests.Day1
 
+open System.IO;
 open NUnit.Framework.Internal
 open NUnit.Framework
 open Day1
+open System
 
 [<TestFixture>]
-type Class1() =     
-    
+type Class1() =
+    let path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "./Day1Input.txt")
+
     [<TestCase(3, "+1, -2, +3, +1")>]
     [<TestCase(3, "+1, +1, +1")>]
     [<TestCase(0, "+1, +1, -2")>]
     [<TestCase(-6, "-1, -2, -3")>]
-    member this.A(exp, input) =
+    member this.Day1AExamples(exp, input) =
        Assert.AreEqual(exp, (day1AExamples input))
-
+    
+    [<Test>]
+    member this.Day1A() =       
+       let input: string[] = File.ReadAllLines(path)
+       Assert.AreEqual(484, (day1A input))
     
     [<TestCase(2, "+1, -2, +3, +1")>]
     [<TestCase(0, "+1, -1")>]
     [<TestCase(10, "+3, +3, +4, -2, -4")>]
     [<TestCase(5, "-6, +3, +8, +5, -6")>]
     [<TestCase(14, "+7, +7, -2, -7, -4")>]
-    member this.B(exp, input) =
+    member this.Day1BExamples(exp, input) =
        Assert.AreEqual(exp, (day1BExamples input))    
+       
+    [<Test>]
+    member this.Day1B() =       
+       let input: string[] = File.ReadAllLines(path)
+       Assert.AreEqual(367, (day1B input))
--- AoC2018/Day1.fs
+++ AoC2018/Day1.fs
@@ -2,22 +2,35 @@
 
 open System
 open System.Collections.Generic
+open System.Collections
+
+type Bits() =
+    let pbits = new BitArray(Int32.MaxValue)
+    let nbits = new BitArray(Int32.MaxValue)
+
+    member this.getBit nr =
+        if nr < 0 then nbits.[-1*nr] else pbits.[nr]    
+    member this.setBit nr =
+        if nr < 0 then nbits.[-1*nr] <- true else pbits.[nr] <- true    
+    member this.unsetBit nr =
+        if nr < 0 then nbits.[-1*nr] <- false else pbits.[nr] <- false
+            
 
 let parseToken (s:String) =    
     match s with
-    | null -> id
+    | null -> failwith "Cannot parse null"
     | "0" -> id
-    | s when s.Length < 2 -> id
+    | s when s.Length < 2 -> failwith (sprintf "Cannot parse %s" s)
     | s ->
         let token = s.Chars 0
         let success, value = Int32.TryParse (s.Substring 1)
         match success, token, value with
-        | false, _, _ -> id
+        | false, _, _ -> failwith (sprintf "Cannot parse %s" s)
         | true, '+', x -> 
             fun(a) -> a + x
         | true, '-', x -> 
             fun(a) -> a - x
-        | true, _, _ -> id
+        | true, _, _ -> failwith (sprintf "Cannot parse %s" s)
 
 let day1A (inputs: String[]) = 
         inputs
@@ -25,36 +38,39 @@ let day1A (inputs: String[]) =
         |> Array.map parseToken 
         |> Array.fold (|>) 0 
 
-let processInput (collector: List<int>) (input, alreadyFoundIt) (operator: int->int) =
+let processInput (input, alreadyFoundIt, collector: Bits) (operator: int->int) =
     let result = operator input
     match alreadyFoundIt with
     | Some x -> 
-        (result, Some x)
+        (result, Some x, collector)
     | None ->
         let foundIt = 
-            if collector.Contains(result) 
-            then Some(result) 
+            if collector.getBit result
+            then 
+                Some(result) 
             else
-                collector.Add(result)
+                collector.setBit result
                 None
-        (result, foundIt)
+        (result, foundIt, collector)
 
 let day1B (inputs: String[]) =    
-    let seenValues = new List<int>()
-    seenValues.Add(0)
+    let ops = 
+        inputs
+            |> Array.map (fun s -> s.Trim())
+            |> Array.map parseToken
+    
+    let numbers = new Bits()
+    numbers.setBit 0
 
-    let mutable current: int = 0
-    let mutable result: int option = None
+    let rec findit state =
+        let (newint, foundIt, foundNumbers) = Array.fold processInput state ops
+        match foundIt with
+        | None -> 
+            findit (newint, foundIt, foundNumbers)
+        | Some x -> 
+            x
 
-    while result = None do
-        let (newint, foundIt) = 
-            inputs
-                |> Array.map (fun s -> s.Trim())
-                |> Array.map parseToken 
-                |> Array.fold (processInput seenValues) (current, result) 
-        current <- newint
-        result <- foundIt
-    result.Value
+    findit (0, None, numbers)
 
 let day1AExamples (s: string) =
     s.Split(',') |> day1A
