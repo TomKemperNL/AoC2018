b3a4c96e3bc766b001fb0e174a2a9a6fef4e92bc
Tom Kemper <tomkempernl@gmail.com>
2018-12-03 19:52:26 +0100

    Day 3 done, mostly by programming C# in F#

---
+++ AoC2018.Tests/Day3Tests.fs
@@ -0,0 +1,35 @@
+﻿module AoC2018.Tests.Day3
+
+open NUnit.Framework
+open System.IO
+open System
+open Day3
+
+[<TestFixture>]
+type Day3Tests() =
+
+    [<Test>]
+    member this.Day3Parse() = 
+        let expected = Claim (12,Coord (2,4), Size (5,6))
+        Assert.AreEqual(expected, parse "#12 @ 2,4: 5x6")
+       
+    [<Test>]
+    member this.Day3AExample() =
+        let path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "./Day3AExample.txt")
+        Assert.AreEqual(4, day3A (File.ReadAllLines(path)))
+    
+    [<Test>]
+    member this.Day3A() =
+        let path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "./Day3Input.txt")
+        Assert.AreEqual(114946, day3A (File.ReadAllLines(path)))
+
+    
+    [<Test>]
+    member this.Day3BExample() =
+        let path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "./Day3AExample.txt")
+        Assert.AreEqual(3, day3B (File.ReadAllLines(path)))
+    
+    [<Test>]
+    member this.Day3B() =
+        let path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "./Day3Input.txt")
+        Assert.AreEqual(877, day3B (File.ReadAllLines(path)))
---
+++ AoC2018/Day3.fs
@@ -0,0 +1,74 @@
+﻿module Day3
+
+open System
+open System.Text.RegularExpressions
+open System.Collections.Generic
+
+type Size =
+    | Size of int*int
+
+[<Struct>]
+type Coord = 
+    | Coord of int*int
+
+type Claim = 
+    | Claim of int*Coord*Size
+
+type Index =
+    Dictionary<Coord, HashSet<Claim>>    
+
+let parse (s:string) : Claim =
+    let regMatch = Regex.Match(s, "\#(\d+) \@ (\d+),(\d+)\: (\d+)x(\d+)")
+    if regMatch.Success then
+        let toDigit (n: int) : int =
+            regMatch.Groups.[n].Value |> Int32.Parse
+        Claim (toDigit 1, Coord(toDigit 2, toDigit 3), Size(toDigit 4, toDigit 5))
+    else failwith(sprintf "Could not parse %s" s)
+
+let addToIndex (index: Index) claim =
+    match claim with
+    | Claim (_, Coord(x,y), Size(w,h)) ->        
+        for ix in x .. (x + w - 1) do
+        for iy in y .. (y + h - 1) do
+        let coord = Coord(ix,iy)
+        if index.ContainsKey(coord) then
+            index.[coord].Add(claim) |> ignore
+        else
+            let freshList : HashSet<Claim> = new HashSet<Claim>()
+            freshList.Add(claim) |> ignore
+            index.Add(coord, freshList) 
+        
+
+let createIndex claims =
+    let index = new Index()
+    for c in claims do
+        addToIndex index c |> ignore
+    index
+
+let day3A (input: string[]) =
+    let parsed = Array.map parse input
+    let index = createIndex parsed    
+    index.Values |> Seq.filter (fun(kv: HashSet<Claim>) -> kv.Count > 1) |> Seq.length
+
+let findPartners (index: Index) (claim: Claim) : HashSet<Claim> =
+    let partners = new HashSet<Claim>()        
+    match claim with
+    | Claim (_, Coord(x,y), Size(w,h)) ->    
+        for ix in x .. (x + w - 1) do
+        for iy in y .. (y + h - 1) do        
+        let coord = Coord (ix, iy)
+        for p in index.[coord] do
+        partners.Add p |> ignore    
+    partners
+
+let day3B (input: string[]) =
+    let parsed = Array.map parse input
+    let index = createIndex parsed 
+    let solo = Array.find (fun(c) -> findPartners index c |> Seq.length = 1) parsed    
+    match solo with 
+    | Claim (id,_,_) -> id
+
+    
+
+
+    
