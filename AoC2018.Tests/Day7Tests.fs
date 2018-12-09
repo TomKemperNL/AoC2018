80b840b7db2ee9ede61b6ec54662bb6c6dffd793
Tom Kemper <tomkempernl@gmail.com>
2018-12-09 20:42:50 +0100

    Day7A

---
+++ AoC2018.Tests/Day7Tests.fs
@@ -0,0 +1,46 @@
+﻿module Day7Tests
+
+open NUnit.Framework
+open System.IO
+open System
+open Day7
+
+[<TestFixture>]
+type Day7Tests() =
+
+    [<Test>]
+    member this.Day7AExample() =       
+        let input : string[] = [| 
+            "Step C must be finished before step A can begin.";
+            "Step C must be finished before step F can begin.";
+            "Step A must be finished before step B can begin.";
+            "Step A must be finished before step D can begin.";
+            "Step B must be finished before step E can begin.";
+            "Step D must be finished before step E can begin.";
+            "Step F must be finished before step E can begin." |]
+        Assert.AreEqual("CABDFE", day7A input)
+
+    [<Test>]
+    member this.Day7A() =
+        let path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "./Day7Input.txt")
+        Assert.AreEqual("BHRTWCYSELPUVZAOIJKGMFQDXN", (day7A (File.ReadAllLines(path))))
+   
+   
+    [<Test>]
+    member this.Day7BExample() =
+        let input : string[] = [| 
+            "Step C must be finished before step A can begin.";
+            "Step C must be finished before step F can begin.";
+            "Step A must be finished before step B can begin.";
+            "Step A must be finished before step D can begin.";
+            "Step B must be finished before step E can begin.";
+            "Step D must be finished before step E can begin.";
+            "Step F must be finished before step E can begin." |]
+        let result = day7B input
+        Assert.AreEqual(null, result)
+
+
+    [<Test>]
+    member this.Day7B() =
+        let path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "./Day7Input.txt")
+        Assert.AreEqual(null, (day7B  (File.ReadAllLines(path))))
---
+++ AoC2018/Day7.fs
@@ -0,0 +1,38 @@
+﻿module Day7
+
+open System.Linq
+
+type Action = char
+type Rule = Action*Action
+
+module Action =
+    let isAvailable rules actionsPerformed action : bool =
+        if Seq.contains action actionsPerformed then false
+        else
+        let unsatisfiedRules = Seq.filter (fun (c,q) -> not <| Seq.contains c actionsPerformed) rules
+        not <| Seq.exists (fun (c,q) -> q = action) unsatisfiedRules
+    
+let parse (input: string) : Rule = 
+    match input with 
+    | Regex "Step ([A-Z]) must be finished before step ([A-Z]) can begin." [a; b] -> (a.Chars 0, b.Chars 0)
+    | _ -> failwith "parse error"
+
+let rec step rules allActions actionsSoFar =    
+    let next = Seq.filter (Action.isAvailable rules actionsSoFar) allActions |> Seq.sort
+    if Seq.isEmpty next then
+        List.rev actionsSoFar
+    else
+        step rules allActions ((Seq.head next) :: actionsSoFar)
+
+let day7A (inputs: string[]) =
+    let rules = inputs |> Seq.map parse
+    let allActions = rules |> Seq.collect (fun (c,r) -> [c; r]) |> Seq.distinct
+    let result: Action list = step rules allActions List.empty
+    new string(result |> List.toArray)
+
+    
+
+
+
+let day7B inputs = 
+    null
