eac8f5f9996fcf25838190f463d38bc62685574d
Tom Kemper <tomkempernl@gmail.com>
2018-12-16 13:15:38 +0100

    Day 8

---
+++ AoC2018.Tests/Day8Tests.fs
@@ -0,0 +1,47 @@
+﻿module Day8Tests
+
+
+open NUnit.Framework
+open System.IO
+open System
+open Day8
+
+[<TestFixture>]
+type Day8Tests() =
+
+    [<Test>]
+    member this.TestParse() =
+        let expected = {
+            Header = Header(1,2)
+            Metadata = [Metadata 2; Metadata 5]
+            Children = [{
+                Header = Header(0,1)
+                Metadata = [Metadata 7]
+                Children = []
+            }]
+        }
+        let input = "1 2 0 1 7 2 5"
+
+        Assert.AreEqual (expected, (parse input))
+
+
+    [<Test>]
+    member this.ExampleA() =
+        let input = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"
+        Assert.AreEqual (138, (day8A input))
+
+    [<Test>]
+    member this.Day8A() =
+        let input = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "./Day8Input.txt"))
+        Assert.AreEqual (43351, (day8A input))
+
+    [<Test>]
+    member this.ExampleB() =
+        let input = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"
+        Assert.AreEqual (66, (day8B input))
+
+    
+    [<Test>]
+    member this.Day8B() =
+        let input = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "./Day8Input.txt"))
+        Assert.AreEqual (21502, (day8B input))
---
+++ AoC2018/Day8.fs
@@ -0,0 +1,80 @@
+﻿module Day8
+
+open System
+
+[<Struct>]
+type Header =
+    | Header of int*int
+
+[<Struct>]
+type Metadata = 
+    | Metadata of int
+
+[<Struct>]
+type Node = {
+    Header: Header;
+    Metadata: Metadata list;
+    Children: Node list
+}
+
+let parse (input: string) : Node =    
+    let rec parseMetadata input nr acc = 
+        match nr with 
+        | 0 -> (List.rev acc, input)
+        | n -> 
+            match input with
+            | h :: t -> parseMetadata t (n-1) ((Metadata h) :: acc)
+            | _ -> failwith "cant make metadate out of this"
+    
+    let parseHeader input = 
+        match input with 
+        | (a:: b:: t) -> (Header(a,b), t)
+        | _ -> failwith "cant make a header out of this"
+    
+    let rec parseChildren input nr acc : (Node list*int list)=
+        match nr with 
+        | 0 -> (List.rev acc, input)
+        | _ ->
+            let (newNode,rest) = parseNode input
+
+            parseChildren rest (nr - 1) (newNode::acc)
+    and parseNode input =
+        let (header, childRest) = parseHeader input
+        match header with 
+        | Header (nrChildren, metaNr) ->
+            let (children, metaRest) = parseChildren childRest nrChildren []
+            let (meta, leftAfterMeta) = parseMetadata metaRest metaNr []
+            let newNode = {
+                Header = header;
+                Metadata = meta;
+                Children = children
+            }        
+            (newNode, leftAfterMeta)
+    let tokens = input.Split(' ') |> Array.map Int32.Parse |> List.ofArray
+    parseNode tokens |> fst
+
+
+let day8A input = 
+    let rec sumNode total (node: Node) = 
+        let mine = List.map (fun (Metadata m) -> m) node.Metadata |> List.sum
+        let theirs = List.fold sumNode 0 node.Children
+        total + mine + theirs
+
+    let tree = parse input
+    sumNode 0 tree
+
+let day8B input = 
+    let rec sumNode (total:int) (node: Node) = 
+        if node.Children.Length = 0 then
+            total + (List.map (fun (Metadata m) -> m) node.Metadata |> List.sum)
+        else
+            let getNode (i: int) :Node= List.item (i-1) node.Children 
+                
+            let childTotal: int = node.Metadata |>
+                                    List.map (fun (Metadata m) -> m) |> 
+                                    List.filter (fun i -> i > 0 && i <= node.Children.Length) |> 
+                                    List.map (fun i -> sumNode 0 (getNode i)) |> 
+                                    List.sum    
+            childTotal + total
+    let tree = parse input
+    sumNode 0 tree
