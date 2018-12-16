4676308bd42d893d42eac0c9d421dc66beaf16e8
Tom Kemper <tomkempernl@gmail.com>
2018-12-16 15:38:54 +0100

    I kinda misunderstood Day9B:)

---
+++ AoC2018.Tests/Day9Tests.fs
@@ -0,0 +1,43 @@
+﻿module Day9Tests
+
+open NUnit.Framework
+open Day9
+
+[<TestFixture>]
+type Day9Tests() =
+    
+    [<Test>]
+    member this.roundAndRound() =
+        let circle = [ 0; 1; 2; 3; 4; 5 ]
+        
+        Assert.AreEqual (5, (Circle.clockwise circle 3 2), "Can go clockwise")
+        Assert.AreEqual (1, (Circle.counterclockwise circle 3 2), "Can go counterclockwise")
+
+        Assert.AreEqual (2, (Circle.clockwise circle 3 5), "Can go round clockwise")
+        Assert.AreEqual (4, (Circle.counterclockwise circle 3 5), "Can go round counterclockwise")
+        
+        Assert.AreEqual (2, (Circle.clockwise circle 3 11), "Can go round clockwise twice")
+        Assert.AreEqual (4, (Circle.counterclockwise circle 3 11), "Can go round counterclockwise twice")
+
+    [<Test>]
+    member this.Day9Examples() =
+        let examples = [|
+            ("9 players; last marble is worth 25 points",    32);
+            ("10 players; last marble is worth 1618 points", 8317);
+            ("13 players; last marble is worth 7999 points", 146373);
+            ("17 players; last marble is worth 1104 points", 2764);
+            ("21 players; last marble is worth 6111 points", 54718);
+            ("30 players; last marble is worth 5807 points", 37305);
+        |]
+        for (input, expected) in examples do
+            Assert.AreEqual(expected, (day9A input), input)
+
+    [<Test>]
+    member this.Day9A() = 
+        let input = "441 players; last marble is worth 71032 points"
+        Assert.AreEqual(393229, (day9A input))
+
+    [<Test>]
+    member this.Day9B() = 
+        let input = "441 players; last marble is worth 7103200 points"
+        Assert.AreEqual(0, (day9A input))
---
+++ AoC2018/Day9.fs
@@ -0,0 +1,94 @@
+﻿module Day9
+
+open System
+open System.Collections.Immutable
+
+type Marble = Marble of int
+
+type Circle = ImmutableList<Marble>
+module Circle =    
+    let private move circle start (modifier: int->int) : int= 
+        let length = Seq.length circle
+        let result = (modifier start) % length        
+        if result >= 0 then result
+        else 
+            length + result
+
+    let clockwise circle (start:int) (steps: int) : int =
+        move circle start ((+) steps)
+    let counterclockwise circle start steps : int =
+        move circle start (fun x -> x - steps)
+
+type GameSettings = {
+    NrOfPlayers: int;
+    LastMarble: int
+}
+
+type GameState = {    
+    Circle: Circle
+    CurrentIndex: int
+}
+
+type Outcome = Map<int, Marble list>
+
+let step (game,outcome) ((Marble n),currentplayer) =
+    if n % 23 =0 then
+        let currentScore = Map.find currentplayer outcome
+        let removePos = Circle.counterclockwise game.Circle game.CurrentIndex 7
+        let extraMarble = game.Circle.Item removePos        
+        let newOutcome = Map.add currentplayer (extraMarble :: Marble n :: currentScore) outcome        
+        let newGame = {
+            game with 
+                Circle = game.Circle.RemoveAt removePos
+                CurrentIndex = removePos
+        }
+        (newGame, newOutcome)
+    else
+        let insertPosition = Circle.clockwise game.Circle game.CurrentIndex 2
+        let newCircle = game.Circle.Insert(insertPosition, Marble n)
+        let newGame = { 
+            game with 
+                Circle = newCircle;
+                CurrentIndex = insertPosition
+        }
+        (newGame, outcome)
+
+let parse input = 
+    match input with 
+    | Regex "(\d+) players; last marble is worth (\d+) points" [players; points] -> 
+        { NrOfPlayers = Int32.Parse players; LastMarble = Int32.Parse points; }
+    | _ -> sprintf "Cant parse %s" input |> failwith 
+
+let runGame marbles players =
+    let infinitePlayers = seq { while true do yield! players }
+    let turns = Seq.zip marbles infinitePlayers
+
+    let initialOutcomes = players |> Seq.map (fun p -> (p, List.empty)) |> Map.ofSeq    
+    let initialGame = {
+        Circle = ImmutableList.Create(Marble 0);
+        CurrentIndex = 0
+    }
+    
+    let (_, finalOutcomes) = Seq.fold step (initialGame, initialOutcomes) turns
+
+    let score (marbles: Marble seq) : int=
+        marbles |> Seq.map (fun (Marble m) -> m) |> Seq.sum
+
+    finalOutcomes |> Map.toSeq |> Seq.map snd |> Seq.map score |> Seq.sortDescending |> Seq.head
+
+let day9A input =
+    let settings = parse input
+    let marbles = seq { 1 .. settings.LastMarble } |> Seq.map Marble
+    let players = seq { 1 .. settings.NrOfPlayers }
+    runGame marbles players
+
+let day9B input =
+    let settings = parse input
+    let marbles =  Seq.map Marble <| seq { 
+        for i in 1 .. (settings.LastMarble - 1) do
+            yield i;        
+        yield settings.LastMarble * 100
+    }
+    let players = seq { 1 .. settings.NrOfPlayers }
+    runGame marbles players   
+
