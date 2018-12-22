482ec25505523a0a88b272f9b2414d178c91822f
Tom Kemper <tomkempernl@gmail.com>
2018-12-22 21:33:05 +0100

    Day11A done, B should be simple, but I'm not seeing it...

---
+++ AoC2018.Tests/Day11Tests.fs
@@ -0,0 +1,24 @@
+﻿module Day11Tests
+
+open NUnit.Framework
+open Day11
+
+[<TestFixture>]
+type Day11Tests() =
+
+    [<Test>]
+    member this.Day11PowerExamples() =
+        Assert.AreEqual(4, FuelCell.power 8 (Cell (3,5)))
+        Assert.AreEqual(-5, FuelCell.power 57 (Cell (122,79)))
+        Assert.AreEqual(0, FuelCell.power 39 (Cell (217,196)))
+        Assert.AreEqual(4, FuelCell.power 71 (Cell (101,153)))
+
+    [<Test>]
+    member this.Day11SquareExamples() =
+        Assert.AreEqual((33,45), day11A 18)
+        Assert.AreEqual((21,61), day11A 42)
+    
+    [<Test>]
+    member this.Day11A() =
+        Assert.AreEqual((20,77), day11A 9221)
+        
---
+++ AoC2018/Day11.fs
@@ -0,0 +1,40 @@
+﻿module Day11
+
+type FuelCell = 
+    | Cell of int*int
+
+module FuelCell = 
+    let power serial (Cell (x,y)) = 
+        //Keep only the hundreds digit of the power level (so 12345 becomes 3; 
+        let hundreds nr = ((nr - (nr % 100)) % 1000) / 100 
+        
+        let rackId = x + 10
+        rackId 
+            |> (*) y
+            |> (+) serial
+            |> (*) rackId
+            |> hundreds 
+            |> fun p -> p - 5
+      
+
+let day11A input = 
+    let cellPower = FuelCell.power input
+    let max = 300
+    let squareSize = 3
+
+    let powergrid = Array2D.initBased 1 1 max max (fun x y -> cellPower (Cell(x,y)))
+
+    let squarePower (topLeftX, topLeftY) =
+        Seq.sum <| seq {
+            for x in topLeftX..(topLeftX + squareSize - 1) do
+            for y in topLeftY..(topLeftY + squareSize - 1) do
+                yield Array2D.get powergrid x y
+        } 
+        
+    let squares = seq {
+        for x in 1 .. (max - squareSize + 1) do
+        for y in 1 .. (max - squareSize + 1) do
+        yield (x,y)
+    }
+
+    Seq.maxBy squarePower squares
